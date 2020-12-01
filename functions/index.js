6106cca6da8e7513ae22e1ea97b5cda2848153d2
Pyrex <retojonatan@gmail.com>
2020-11-30 22:20:13 -0300

    commit ordenando el firebase con las functions y hosting

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,27 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin')
 const express = require('express')
-const serviceAccount = require('./credentials.json')
 const app = express()
+var admin = require("firebase-admin");
+var serviceAccount = require("./cert.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://leanim-crm-83458.firebaseio.com"
-})
-
-const db = admin.firestore()
+  databaseURL: "https://fir-leanim.firebaseio.com"
+});
 
 app.get('/clientes/:id', (req, res) => {
-  try {
-    db.collection('clientes').doc(req.params.id).get()
-      .then(doc => {
-        let cliente = doc.data()
-        return res.send(cliente)
-      })
-  } catch (error) {
-    console.error(error)
-  }
+  db.collection('clientes')
+    .doc(req.params.id)
+    .get()
+    .then(cliente => {
+      let datos = cliente.data()
+      return res.send(datos)
+    })
+    .catch(err => console.error(err))
 })
 
-app.post('/api/products', async (req, res) => {
-  await db.collection('products')
-    .doc('/' + req.body.id + '/')
-    .create({
-      name: req.body.name
-    })
-  return res.status(204).json()
+app.get('/hola', (req, res) => {
+  return res.send('HOLAAAAAAAAAAAAAA')
 })
 
 exports.app = functions.https.onRequest(app)
